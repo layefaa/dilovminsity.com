088e0485984a3880fe9b0a450b9a9eb929fce4ba
Martin-Luther Obahor <kingm4159@gmail.com>
2023-05-18 23:55:55 +0100

    added contents

--- src/constants/content.ts
+++ src/constants/content.ts
@@ -2,3 +2,28 @@ export const homePage = {
   header: 'AN ABUNDANT, ADVENTUROUS LIFE AWAITS',
   heroParagraph: 'Put aside the stress of worldly expectation and follow God’s path to a life of abundance.'
 }
+
+
+export const mission = {
+  header: 'Our mission',
+  paragraph: ' Dilovministry   is set to uphold God\'s standard and precepts. Propagating the gospel of Truth, Love, and Hope to mankind.'
+}
+
+export const about = {
+  header: 'About Us',
+  paragraph: 'It is a community  of talented and gifted individuals who are ready to transform the world to a better place. It was envisioned to save lives both locally and globally through the act of love. It focuses on enriching the spiritual and physical lives of people towards God, and ensuring a life filled with love and purpose.'
+}
+
+export const objectives = {
+  header: 'Objectives',
+  list: [
+    'Reach out to lost soul through evangelism and fellowship.',
+    'Contribute to the walfare of people both locally and global through charity and love, and exhortation.',
+    'Establish the right standards of living.',
+    'Promote a godly life in nations around the world.'
+  ]
+}
+
+export const volunteerPage = {
+  callToAction: 'Join Our Team and Make a Difference!',
+}
