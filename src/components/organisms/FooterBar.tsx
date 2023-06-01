897c2c88743b9b056fb42ee98375c9509aedf2c6
Martin-Luther Obahor <kingm4159@gmail.com>
2023-06-01 22:04:04 +0100

    update on files"

--- src/components/organisms/FooterBar.tsx
+++ src/components/organisms/FooterBar.tsx
@@ -3,7 +3,7 @@ import {Socials} from "@/components/molecules";
 const FooterBar = () => {
   return (
       <div className={'flex justify-between text-16 text-dm-secondary-white'}>
-        <div className={'inline-flex flex-col md:flex-col gap-2 md:gap-10'}>
+        <div className={'inline-flex flex-col md:flex-row gap-2 md:gap-10'}>
           <p>Copyright © 2023 Dilov Ministry</p>
           <p>Privacy Policy</p>
         </div>
