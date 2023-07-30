b461265c93dd8578cfbe92a0f7598d36ec6003ee
Martin-Luther Obahor <kingm4159@gmail.com>
2023-07-30 17:13:48 +0100

    select

--- src/components/molecules/SelectField.tsx
+++ src/components/molecules/SelectField.tsx
@@ -9,49 +9,49 @@ import {AnimatePresence} from "framer-motion";
 const inputStyle = 'appearance-none px-[1.6rem] py-[1rem] font-[400] text-16 placeholder:text-16 placeholder:text-dm-tertiary-white2 rounded-lg outline-none relative bg-transparent overflow-hidden  text-dm-secondary-white border-ce-tertiary-white border-dm-tertiary-white2 border inline-flex rounded-[5px]'
 
 const SelectField = ({label, placeholder, validation, id, options}: IInput) => {
-  const {
-    register,
-    formState: {errors},
-  } = useFormContext()
+    const {
+        register,
+        formState: {errors},
+    } = useFormContext()
 
-  const inputError = findInputError(errors, id)
-  const isInvalid = isFormInvalid(inputError)
+    const inputError = findInputError(errors, id)
+    const isInvalid = isFormInvalid(inputError)
 
-  // @ts-ignore
+    // @ts-ignore
 
-  return (
-      <div className={'mb-[1.5rem] sm:mb-[2rem] lg:mb-[3rem] flex flex-col w-full'}>
-        <div className={'flex justify-between items-center'}>
-          <Label label={label}/>
-          <AnimatePresence mode="wait" initial={false}>
-            {isInvalid && (
-                <ErrorMessage
+    return (
+        <div className={'mb-[1.5rem] sm:mb-[2rem] lg:mb-[3rem] flex flex-col w-full'}>
+            <div className={'flex justify-between items-center'}>
+                <Label label={label}/>
+                <AnimatePresence mode="wait" initial={false}>
+                    {isInvalid && (
+                        <ErrorMessage
+                            // @ts-ignore
+                            error={inputError.error.message}
+                            // @ts-ignore
+                            key={inputError.error.message}
+                        />
+                    )}
+                </AnimatePresence>
+            </div>
+            <select placeholder={placeholder}
+                    className={` ${inputStyle}`}
+                    {...register(id, validation)}
+            >
+                <option value={''} selected >{placeholder}</option>
+                {
+
                     // @ts-ignore
-                    error={inputError.error.message}
-                    // @ts-ignore
-                    key={inputError.error.message}
-                />
-            )}
-          </AnimatePresence>
+                    options.map((x) => {
+                        return (
+                            <option key={x.name} value={x.name} className={'appearance-none capitalize bg-transparent'}> {x.name}</option>
+                        )
+                    })
+                }
+            </select>
+
         </div>
-        <select placeholder={placeholder}
-                className={` ${inputStyle}`}
-                {...register(id, validation)}
-        >
-            <option value={''} selected>{placeholder}</option>
-          {
-
-            // @ts-ignore
-            options.map((x) => {
-              return (
-                  <option key={x.name} value={x.name} className={'capitalize'}> {x.name}</option>
-              )
-            })
-          }
-        </select>
-
-      </div>
-  );
+    );
 };
 
 export default SelectField;
