6906e4ad3c5da6a7fef76cab7e6da43d383652aa
Martin-Luther Obahor <layefa@Martin-Luthers-MacBook-Pro.local>
2023-09-17 15:04:46 +0100

    opacity

--- src/app/(website)/layout.tsx
+++ src/app/(website)/layout.tsx
@@ -1,25 +1,52 @@
 "use client"
-import React from "react";
+import React, {useEffect} from "react";
 import MenuContextProvider from "@/context/MenuContextProvider";
 import {domAnimation, LazyMotion} from "framer-motion";
 import {CabinetGrotesk} from "@/fonts";
 import {Footer, Header} from "@/components/templates";
 
+// @ts-ignore
+const OpacityReducer = ({startDate}) => {
+    useEffect(() => {
+        // Calculate the time difference between the current date and the start date
+        const currentDate = new Date();
+        const startTime = new Date(startDate);
+        // @ts-ignore
+        const timeDifference = currentDate - startTime;
+
+        // Calculate the number of 24-hour periods elapsed
+        const daysElapsed = Math.floor(timeDifference / (24 * 60 * 60 * 1000));
+
+        // Calculate the new opacity (1/7 reduction for each 24-hour period)
+        const newOpacity = 1 - (daysElapsed / 7);
+
+        // Apply the new opacity to document.body (ensure it doesn't go below 0)
+        document.body.style.opacity = String(Math.max(newOpacity, 0));
+
+        // Save the new opacity to localStorage for the next visit
+        localStorage.setItem('opacity', newOpacity.toString());
+    }, [startDate]);
+
+    return null;
+};
+
 
 export default function RootLayout({children,}: { children: React.ReactNode }) {
-  return (
-      <html lang="en">
-      <body>
-      <MenuContextProvider>
-        <LazyMotion features={domAnimation}>
-          <Header/>
-          <main className={`${CabinetGrotesk.className} mb-[3.5rem] md:mb-[7.5rem] leading-[120%] text-20 w-full  `}>
-            {children}
-          </main>
-          <Footer/>
-        </LazyMotion>
-      </MenuContextProvider>
-      </body>
-      </html>
-  )
+    return (
+        <html lang="en">
+        <body>
+        <MenuContextProvider>
+            <LazyMotion features={domAnimation}>
+                <Header/>
+                <OpacityReducer startDate="2023-09-16T00:00:00"/>
+                <main
+                    className={`${CabinetGrotesk.className} mb-[3.5rem] md:mb-[7.5rem] leading-[120%] text-20 w-full  `}>
+                    {children}
+                </main>
+                <Footer/>
+            </LazyMotion>
+        </MenuContextProvider>
+        </body>
+        </html>
+    )
 }
